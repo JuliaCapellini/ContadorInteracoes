8bc3efa1a7872fdf48f23d45d8e2b5a9cefc596c
Julia Capellini <163786516+JuliaCapellini@users.noreply.github.com>
2025-05-07 16:27:20 -0300

    AdicionandoIndexScript

---
+++ script.js
@@ -0,0 +1,17 @@
+let contador = 0
+let res = document.querySelector('section#result')
+
+function contar() {
+    contador ++
+    res.innerHTML = <p>O contador está com <mark>${contador}</mark> cliques.</p>
+}
+
+function diminuir() {
+    contador --
+    res.innerHTML = <p>O contador está com <mark>${contador}</mark> cliques.</p>
+}
+
+function zerar() {
+    contador = 0
+    res.innerHTML = null
+}
